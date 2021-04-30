529e2022b7aa51b32286087dd4663c1e5875cab9
Ivan Tomica <ivan@tomica.net>
2021-04-30 06:17:52 +0200

    Add lambda function

---
+++ lambda_function.py
@@ -0,0 +1,39 @@
+import re
+
+def lambda_handler(event, context):
+    request = event['Records'][0]['cf']['request']
+    uri = request['uri']
+
+    if re.match('.+/$', uri):
+        request['uri'] = uri + "index.html"
+        return request
+
+    elif re.match('(.+)/index.html', uri):
+        prefix_path = re.search('(.+)/index.html', uri)
+        response = {
+            'status': '301',
+            'statusDescription': 'Found',
+            'headers': {
+                'location': [{
+                    'key': 'Location',
+                    'value': prefix_path[1] + '/'
+                }]
+            }
+        }
+        return response
+
+    elif re.match('/[^/.]+$', uri):
+        response = {
+            'status': '301',
+            'statusDescription': 'Found',
+            'headers': {
+                'location': [{
+                    'key': 'Location',
+                    'value': uri + '/'
+                }]
+            }
+        }
+        return response
+
+    else:
+        return request
